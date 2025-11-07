4d074381cae43bab948d329e080dae4d6ae247ea
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2025-11-07 18:15:57 +0600

    feat(movie-finder): add movie fetching functionality with loading and error states
    - Implement API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+};
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [movieList, setMovieList] = useState([]);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage("");
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+            if (!response.ok) {
+                throw new Error("Failed to fetch movies. Please try again.");
+            }
+
+            const data = await response.json();
+            if (data.Response === 'False' || data.results.length === 0) {
+                throw new Error("No movies found. Please try a different search term.");
+            }
+
+
+            setMovieList(data.results || []);
+            console.log(data.results);
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+            setErrorMessage("Failed to fetch movies. Please try again.");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
 
     return (
         <main>
@@ -17,7 +65,21 @@ const App = () => {
 
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
-                <h1 className="text-3xl font-bold underline text-center">Movie Finder</h1>
+
+                <section className="all-movies">
+                    <h2>All Movies</h2>
+
+                    {isLoading ? (
+                        <Spinner/>
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        // movieList.map((movie) => (
+                        //     <MovieCard key={movie.id} movie={movie} />
+                                // ))
+                                <MovieCard />
+                    )}
+                </section>
             </div>
         </main>
     );
