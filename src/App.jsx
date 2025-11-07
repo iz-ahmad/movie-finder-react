4adfe2912bd7b3b4bd973d7d3e931286794966b6
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2025-11-07 17:04:20 +0600

    feat: add search component and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import Search from "./components/Search.jsx";
 
 const App = () => {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline text-center">
-        Movie Finder
-      </h1>
-    </div>
-  )
-}
+    const [searchTerm, setSearchTerm] = useState("");
 
-export default App
+    return (
+        <main>
+            <div className="pattern"></div>
+
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle!
+                    </h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                </header>
+                <h1 className="text-3xl font-bold underline text-center">Movie Finder</h1>
+            </div>
+        </main>
+    );
+};
+
+export default App;
